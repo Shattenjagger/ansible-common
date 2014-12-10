49950233eb8e6341aa2199da12385d9cf5a4fe9e
Danila Balabaev <shattenjagger@yandex.ru>
2014-12-10 22:00:42 +0300

    Minor parameters change

--- deploy_server.sh
+++ deploy_server.sh
@@ -3,5 +3,5 @@
 BASEDIR=$(dirname $0)
 APP_NAME=$2
 $BASEDIR/parse_current_settings.py $1 $2
-ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/deployment.yaml -l $1 --extra-vars '{"django_app": "${APP_NAME}"}'
-ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/django_routines.yaml -l $1  --extra-vars '{"django_app": "${APP_NAME}"}'
+ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/deployment.yaml -l $1 --extra-vars "{\"django_app\": \"$APP_NAME\"}"
+ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/django_routines.yaml -l $1  --extra-vars "{\"django_app\": \"APP_NAME\"}"
