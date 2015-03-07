6b8bb9b00a519c1dd1f8e7227b0e4399691c8bcb
Danila Balabaev <shattenjagger@yandex.ru>
2015-03-07 19:01:15 +0300

    Another parental support

--- deploy_webapp_server.sh
+++ deploy_webapp_server.sh
@@ -4,3 +4,4 @@ BASEDIR=$(dirname $0)
 APP_NAME=$2
 DJANGO_DIR=$3
 $BASEDIR/parse_current_settings.py $1 $2 $3
+ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/django_webapp_deployment.yaml -l $1 --extra-vars "{\"django_app\": \"$APP_NAME\"}"
