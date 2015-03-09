5a3696a49b5ef934d4b577bd677d77cc6878966c
Danila Balabaev <shattenjagger@yandex.ru>
2015-03-09 19:28:20 +0300

    Nginx reconf

--- deploy_webapp_server.sh
+++ deploy_webapp_server.sh
@@ -6,3 +6,4 @@ DJANGO_DIR=$3
 $BASEDIR/parse_current_settings.py $1 $2 $3
 
 ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/django_webapp_deployment.yaml -l $1 --extra-vars "{\"django_app\": \"$APP_NAME\"}"
+ansible-playbook -i $BASEDIR/../inventory.ansible $BASEDIR/django_routines.yaml -l $1  --extra-vars "{\"django_app\": \"$APP_NAME\"}"
